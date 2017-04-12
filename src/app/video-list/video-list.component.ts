f9424c35602d707a9994e15015f99242d2a78080
CFE <hello@teamcfe.com>
2017-04-12 12:16:13 -0700

    5 - Selectors & Components

--- src/app/video-list/video-list.component.ts
+++ src/app/video-list/video-list.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./video-list.component.css']
 })
 export class VideoListComponent implements OnInit {
-
+    title = "Something";
   constructor() {}
 
   ngOnInit() {
