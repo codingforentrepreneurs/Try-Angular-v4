328fd334dffc557980d7c2a69e1c78a654ee9df9
CFE <hello@teamcfe.com>
2017-04-13 10:36:41 -0700

    12 - Bootstrap for Angular - ngx-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+// third party imports
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+
+
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing';
 import { VideoListComponent } from './video-list/video-list.component';
@@ -18,6 +23,7 @@ import { SafePipe } from './utility/safe.pipe';
     SafePipe
   ],
   imports: [
+    BsDropdownModule.forRoot(),
     BrowserModule,
     FormsModule,
     HttpModule,
