fd53b48b41bc045911d4a52a1f2f218bc0e3c66a
CFE <hello@teamcfe.com>
2017-04-13 11:21:01 -0700

    14 - Angular Click Events

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+   homeImageList = [
+        {image: "assets/images/nature/4.jpg", title:"Image 4", link:'/videos/video-1'},
+        {image: "assets/images/nature/5.jpg", title:"Image 5", link:'/videos/video-1'},
+        {image: "assets/images/nature/6.jpg", title:"Image 6", link:'/videos/video-1'},
+        {image: "assets/images/nature/1.jpg", title:"Image 1", link:'/videos/video-1'}
 
-  constructor() { }
+   ]
+  constructor(private router:Router) { }
 
   ngOnInit() {
+  }
+
+  preventNormal(event:MouseEvent, image:any){
+      if (!image.prevented){
+          event.preventDefault()
+          //image.setAttribute("href", "/videos")
+          //image.link = '/videos'
+          //image.prevented = true;
+          this.router.navigate(['./videos'])
+          
+      }
   }
 
 }
