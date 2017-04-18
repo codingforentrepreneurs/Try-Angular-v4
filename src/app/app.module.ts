9c6cb472c8bda8ce966ec352b3adfb900cbd70d9
CFE <hello@teamcfe.com>
2017-04-18 09:29:33 -0700

    17 - Two Way Data Binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HomeComponent } from './home/home.component';
 import { SafePipe } from './utility/safe.pipe';
 import { VideoListComponent } from './video-list/video-list.component';
 import { VideoDetailComponent } from './video-detail/video-detail.component';
+import { SearchComponent } from './search/search.component';
 
 
 
@@ -24,7 +25,8 @@ import { VideoDetailComponent } from './video-detail/video-detail.component';
     VideoListComponent,
     VideoDetailComponent,
     SafePipe,
-    HomeComponent
+    HomeComponent,
+    SearchComponent
   ],
   imports: [
     // ngx-bootstrap
