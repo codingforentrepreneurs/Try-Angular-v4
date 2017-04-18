b87133b8f8f12462f8f12e1ab9515c7e894807fd
CFE <hello@teamcfe.com>
2017-04-18 09:10:19 -0700

    16 - Http & Featured

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,22 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+    private req: any;
    homeImageList = [
-        {image: "assets/images/nature/4.jpg", title:"Image 4", link:'/videos/video-1'},
-        {image: "assets/images/nature/5.jpg", title:"Image 5", link:'/videos/video-1'},
-        {image: "assets/images/nature/6.jpg", title:"Image 6", link:'/videos/video-1'},
-        {image: "assets/images/nature/1.jpg", title:"Image 1", link:'/videos/video-1'}
+        // {image: "assets/images/nature/4.jpg", name:"Image 4", slug:'video-1'},
+        // {image: "assets/images/nature/5.jpg", name:"Image 5", link:'video-1'},
+        // {image: "assets/images/nature/6.jpg", name:"Image 6", link:'video-1'}
 
    ]
-  constructor(private router:Router) { }
+  constructor(private http:Http, private router:Router) { }
 
   ngOnInit() {
+      this.req = this.http.get('assets/json/videos.json').subscribe(data=>{
+          console.log(data.json())
+          data.json().filter(item=>{
+              if(item.featured){
+                  this.homeImageList.push(item)
+              }
+          })
+          // this.homeImageList = data.json();
+       })
+  }
+
+  ngOnDestroy(){
+      this.req.unsubscribe()
   }
 
   preventNormal(event:MouseEvent, image:any){
