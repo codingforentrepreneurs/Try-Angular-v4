703f3b2a4e8d6ebd86dc923339fab5a26f79ac8d
CFE <hello@teamcfe.com>
2017-04-18 09:02:23 -0700

    15 - Http Requests

--- src/app/video-detail/video-detail.component.ts
+++ src/app/video-detail/video-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'video-detail',
@@ -8,17 +9,32 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class VideoDetailComponent implements OnInit, OnDestroy {
     private routeSub:any;
+    private req:any;
+    // video = {
+    //      name: "Default",
+    //      slug: "item-1",
+    //      embed: "1hyjLD7pk10"
+    // }
+    video: any;
     slug:string;
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private http:Http) { }
 
   ngOnInit() {
       this.routeSub = this.route.params.subscribe(params => {
           console.log(params)
           this.slug = params['slug']
+          this.http.get('assets/json/videos.json').subscribe(data=>{
+              data.json().filter(item=>{
+                  if (item.slug == this.slug) {
+                      this.video = item
+                  }
+              })
+          })
       })
   }
   ngOnDestroy(){
       this.routeSub.unsubscribe()
+      this.req.unsubscribe()
   }
 
 }
--- src/app/video-list/video-list.component.ts
+++ src/app/video-list/video-list.component.ts
@@ -1,37 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'video-list',
   templateUrl: './video-list.component.html',
   styleUrls: ['./video-list.component.css']
 })
-export class VideoListComponent implements OnInit {
+export class VideoListComponent implements OnInit, OnDestroy {
+    private req: any;
     title = "Video List";
     someItem = "<h1>Hi there</h1>"
     todayDate; // https://angular.io/docs/ts/latest/guide/pipes.html
-
-    videoList = [
-        {
-         name: "Item 1",
-         slug: "item-1",
-         embed: `1hyjLD7pk10`,
-        },
-        {
-         name: "Item 2",
-         slug: "item-2",
-         embed: `1hyjLD7pk10`,
-        },
-        {
-         name: "Item 3",
-         slug: "item-3",
-         embed: null,
-        }
-    ]
-  constructor() {}
+    videoList: [any];
+  constructor(private http:Http) {}
 
   ngOnInit() {
     this.todayDate = new Date()
+    this.req = this.http.get('assets/json/videos.json').subscribe(data=>{
+      console.log(data.json())
+      this.videoList = data.json() as [any];
+    })
+
+  }
+
+  ngOnDestroy(){
+    this.req.unsubscribe()
   }
 
   getEmbedUrl(item){
